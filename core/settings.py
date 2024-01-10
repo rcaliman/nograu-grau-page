2212b9f05df9368df583ef33e0b2a7a6924228e9
Rodrigo Caliman <rcaliman@gmail.com>
2024-01-10 16:13:57 -0300

    csrf

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = config(
 #CSRF_TRUSTED_ORIGINS = config(
 #    "CSRF_TRUSTED_ORIGIN", cast=lambda x: [s.strip() for s in x.split(",")]
 #)
-CSRF_TRUSTED_ORIGINS = ['https://www.nograu.com.br',]
+CSRF_TRUSTED_ORIGINS = ['https://www.nograu.com.br','*127.0.0.1']
 
 
 # Application definition
