584d7313fa4a1815d50ef06ac44aee5c6ad0b76f
Rodrigo Caliman <rcaliman@gmail.com>
2024-03-08 08:29:40 -0300

    ajuste dos inputs

--- apps/cafe/forms.py
+++ apps/cafe/forms.py
@@ -4,10 +4,10 @@ from .models import ProdutorModel, CafeModel, TorraModel
 
 class LoginForm(forms.Form):
     username = forms.CharField(
-        max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'})
+        max_length=100, widget=forms.TextInput(attrs={"class": "form-control"})
     )
     password = forms.CharField(
-        max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control'})
+        max_length=100, widget=forms.PasswordInput(attrs={"class": "form-control"})
     )
 
 
@@ -16,20 +16,20 @@ class ProdutorForm(forms.ModelForm):
         model = ProdutorModel
         fields = "__all__"
         widgets = {
-            'nome': forms.TextInput(attrs={'class': 'form-control'}),
-            'localidade': forms.TextInput(attrs={'class': 'form-control'}),
-            'cidade': forms.TextInput(attrs={'class': 'form-control'}),
-            'estado': forms.TextInput(attrs={'class': 'form-control'}),
-            'telefone': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            "nome": forms.TextInput(attrs={"class": "form-control"}),
+            "localidade": forms.TextInput(attrs={"class": "form-control"}),
+            "cidade": forms.TextInput(attrs={"class": "form-control"}),
+            "estado": forms.TextInput(attrs={"class": "form-control"}),
+            "telefone": forms.TextInput(attrs={"class": "form-control"}),
+            "email": forms.EmailInput(attrs={"class": "form-control"}),
         }
         labels = {
-            'nome': 'Nome do Produtor:',
-            'localidade': 'Localidade:',
-            'cidade': 'Cidade:',
-            'estado': 'Estado:',
-            'telefone': 'Telefone:',
-            'email': 'E-mail:',
+            "nome": "Nome do Produtor:",
+            "localidade": "Localidade:",
+            "cidade": "Cidade:",
+            "estado": "Estado:",
+            "telefone": "Telefone:",
+            "email": "E-mail:",
         }
 
 
@@ -38,47 +38,48 @@ class CafeForm(forms.ModelForm):
         model = CafeModel
         fields = "__all__"
         widgets = {
-            'especie': forms.TextInput(attrs={'class': 'form-control'}),
-            'variedade': forms.TextInput(attrs={'class': 'form-control'}),
-            'altitude': forms.NumberInput(attrs={'class': 'form-control'}),
-            'secagem': forms.TextInput(attrs={'class': 'form-control'}),
-            'preco': forms.NumberInput(attrs={'class': 'form-control'}),
-            'produtor': forms.TextInput(attrs={'class': 'form-control'}),
+            "especie": forms.TextInput(attrs={"class": "form-control"}),
+            "variedade": forms.TextInput(attrs={"class": "form-control"}),
+            "altitude": forms.NumberInput(attrs={"class": "form-control"}),
+            "secagem": forms.TextInput(attrs={"class": "form-control"}),
+            "preco": forms.NumberInput(attrs={"class": "form-control"}),
+            "produtor": forms.Select(attrs={"class": "form-control"}),
         }
         labels = {
-            'especie': 'Espécie:',
-            'variedade': 'Variedade:',
-            'altitude': 'Altitude:',
-            'secagem': 'Secagem:',
-            'preco': 'Preço:',
-            'produtor': 'Produtor:',
+            "especie": "Espécie:",
+            "variedade": "Variedade:",
+            "altitude": "Altitude:",
+            "secagem": "Secagem:",
+            "preco": "Preço:",
+            "produtor": "Produtor:",
         }
+
 
 class TorraForm(forms.ModelForm):
     class Meta:
         model = TorraModel
         fields = "__all__"
         widgets = {
-            'temperatura_inicial': forms.NumberInput(attrs={'class': 'form-control'}),
-            'temperatura_final': forms.NumberInput(attrs={'class': 'form-control'}),
-            'temperatura_piso': forms.NumberInput(attrs={'class': 'form-control'}),
-            'fluxo_ar': forms.NumberInput(attrs={'class': 'form-control'}),
-            'velocidade_tambor': forms.NumberInput(attrs={'class': 'form-control'}),
-            'peso': forms.NumberInput(attrs={'class': 'form-control'}),
-            'data_torra': forms.DateInput(attrs={'class': 'form-control'}),
-            'dados_torra': forms.HiddenInput(),
-            'cafe': forms.Select(attrs={'class': 'form-control'}),
-            'observacoes': forms.TextInput(attrs={'class': 'form-control'}),
+            "temperatura_inicial": forms.NumberInput(attrs={"class": "form-control"}),
+            "temperatura_final": forms.NumberInput(attrs={"class": "form-control"}),
+            "temperatura_piso": forms.NumberInput(attrs={"class": "form-control"}),
+            "fluxo_ar": forms.NumberInput(attrs={"class": "form-control"}),
+            "velocidade_tambor": forms.NumberInput(attrs={"class": "form-control"}),
+            "peso": forms.NumberInput(attrs={"class": "form-control"}),
+            "data_torra": forms.DateInput(attrs={"class": "form-control",  "type": "date"}),
+            "dados_torra": forms.HiddenInput(),
+            "cafe": forms.Select(attrs={"class": "form-control"}),
+            "observacoes": forms.TextInput(attrs={"class": "form-control"}),
         }
         labels = {
-            'temperatura_inicial': 'Tempratura inicial:',
-            'temperatura_final': 'Temperatura final:',
-            'temperatura_piso': 'Temperatura mais baixa:',
-            'fluxo_ar': 'Fluxo de ar:',
-            'velocidade_tambor': 'Velocidade do tambor:',
-            'peso': 'Peso:',
-            'data_torra': 'Data da torra:',
-            'dados_torra': '',
-            'cafe': 'Café:',
-            'observacoes': 'Observações:',
+            "temperatura_inicial": "Tempratura inicial:",
+            "temperatura_final": "Temperatura final:",
+            "temperatura_piso": "Temperatura mais baixa:",
+            "fluxo_ar": "Fluxo de ar:",
+            "velocidade_tambor": "Velocidade do tambor:",
+            "peso": "Peso:",
+            "data_torra": "Data da torra:",
+            "dados_torra": "",
+            "cafe": "Café:",
+            "observacoes": "Observações:",
         }
