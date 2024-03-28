4e0cc47e797ca41730d03754a2d9323682a4cb5f
Rodrigo Caliman <rcaliman@gmail.com>
2024-03-28 14:06:26 -0300

    ajustes no calculo de horas

--- apps/horas/forms.py
+++ apps/horas/forms.py
@@ -7,7 +7,7 @@ jornada_choices = [
 
 
 class FormHoras(forms.Form):
-    entrada = forms.TimeField(required=True, widget=forms.TextInput(attrs={'type': 'time'}))
-    almoco = forms.TimeField(required=False, widget=forms.TimeInput(attrs={'type': 'time'}))
-    volta_almoco = forms.TimeField(required=False, widget=forms.TimeInput(attrs={'type': 'time'}))
-    jornada = forms.ChoiceField(choices=jornada_choices, required=True)
+    entrada = forms.TimeField(required=True, label='Entrada:', widget=forms.TextInput(attrs={'type': 'time'}))
+    almoco = forms.TimeField(required=False, label='Almoço:', widget=forms.TimeInput(attrs={'type': 'time'}))
+    volta_almoco = forms.TimeField(required=False, label='Volta do almoço:', widget=forms.TimeInput(attrs={'type': 'time'}))
+    jornada = forms.ChoiceField(choices=jornada_choices, label='Jornada:', required=True)
